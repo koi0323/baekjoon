f60991140f9ef816d29c5bb7579e35f0b2534737
jkil <wodud0323@gmail.com>
2025-10-19 20:38:56 +0900

    modified set

--- cpp/Set.cpp
+++ cpp/Set.cpp
@@ -7,50 +7,54 @@
 
 using namespace std;
 
+int Cmp(char *a, char *b) {
+    int i = 0;
+    while (a[i] != 0 && b[i] != 0) {
+        if (a[i] != b[i])
+            return (0);
+        ++i;
+    }
+    if (a[i] != b[i])
+        return (0);
+    else
+        return (1);
+}
 int main(void) {
     int nCount;
-    map<int, int> m;
+    bool arr[21] = {false};
     scanf("%d", &nCount);
     for (int i = 0; i < nCount; i++) {
-        string a;
-        cin >> a;
-        if (a == "add") {
+        char tmp[7] = {0,};
+        scanf("%s", tmp);
+        if (Cmp(tmp, "add")) {
             int n;
             scanf ("%d", &n);
-            m.insert(make_pair(n, n));
+            arr[n] = true;
         }
-        else if (a == "remove") {
+        else if (Cmp(tmp, "remove")) {
             int n;
             scanf("%d", &n);
-            m.erase(n);
+            arr[n] = false;
         }
-        else if (a == "check") {
+        else if (Cmp(tmp, "check")) {
             int n;
             scanf("%d", &n);
-            auto it = m.insert(make_pair(n, n));
-            if (it.second) {
-                m.erase(n);
-                printf("0\n");
-            }
-            else
-                printf("1\n");
+            arr[n] ? printf("1\n") : printf("0\n");
         }
-        else if (a == "toggle") {
+        else if (Cmp(tmp, "toggle")) {
             int n;
             scanf("%d", &n);
-            auto it = m.insert(make_pair(n, n));
-            if (!it.second) {
-                m.erase(n);
-            }
+            arr[n] = !arr[n];
         }
-        else if (a == "all") {
-            m.clear();
-            for (int i = 0; i < 20; i++) {
-                m.insert(make_pair(i + 1, i + 1));
+        else if (Cmp(tmp, "all")) {
+            for (int j = 0; j < 21; j++) {
+                arr[j] = true;
             }
         }
         else {
-            m.clear();
+            for (int j = 0; j < 21; j++) {
+                arr[j] = false;
+            }
         }
     }
 }
