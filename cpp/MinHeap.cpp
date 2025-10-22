c328b81d24d666af58c53854546c330c05d71610
jkil <wodud0323@gmail.com>
2025-10-22 15:43:00 +0900

    added 1 problem

--- cpp/MinHeap.cpp
+++ cpp/MinHeap.cpp
@@ -8,22 +8,26 @@ using namespace std;
 
 int main(void) {
     int n;
-    scanf ("%d", &n);
+    map<int, int> m;
+    scanf("%d", &n);
     for (int i = 0; i < n; i++) {
         int tmp;
-        map <int, int> m;
-        scanf ("%d", &tmp);
+        scanf("%d", &tmp);
         if (tmp == 0) {
             if (m.empty())
                 printf("0\n");
             else {
-                auto it = m.begin();
-                printf("%d\n", m.at(0));
-                m.erase (it);
-                m.insert(make_pair(tmp, tmp));
+                printf("%d\n", m.begin()->first);
+                if (m.begin()->second != 1)
+                    m.begin()->second -= 1;
+                else
+                    m.erase(m.begin());
             }
         }
-        else
-            m.insert(make_pair(tmp, tmp));
+        else {
+            auto it = m.insert(make_pair(tmp, 1));
+            if (!it.second)
+                m[tmp]++;
+        }
     }
 }
