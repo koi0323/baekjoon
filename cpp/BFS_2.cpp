4f671c9dd5a10cd14aeee7ebfb6f55a47bf6ffec
Jaeyeong Kil <jkil@c1r2s2.42gyeongsan.kr>
2025-10-27 07:42:52 +0900

    renamed 1 file

--- cpp/BFS_2.cpp
+++ cpp/BFS_2.cpp
@@ -4,25 +4,13 @@
 
 #include <iostream>
 #include <vector>
+#include <queue>
 //queue를 사용한 bfs로 수정하기
 using namespace std;
 
-void dijkstra(vector<vector<int>> &board, vector<vector<int>> &result, int i, int j, int before) {
-    if (i < 0 || j < 0 || i >= board.size() || j >= board[0].size())
-        return ;
-    if (board[i][j] == 0)
-        return ;
-    if (before + 1 > result[i][j])
-        return ;
-    result[i][j] = before + 1;
-    dijkstra(board, result, i - 1, j, before + 1);
-    dijkstra(board, result, i + 1, j, before + 1);
-    dijkstra(board, result, i, j - 1, before + 1);
-    dijkstra(board, result, i, j + 1, before + 1);
-
-}
 int main(void) {
     int n, m;
+    queue<pair<int, int>> q;
     scanf("%d %d", &n, &m);
     vector<vector<int>> board(n, vector<int>(m));
     vector<vector<int>> result(n, vector<int>(m));
@@ -36,13 +24,26 @@ int main(void) {
             if (board[i][j] == 2) {
                 start.first = i;
                 start.second = j;
+                q.push(start);
             }
             if (board[i][j] == 0)
                 result[i][j] = 0;
         }
     }
-    result[start.first][start.second] = 0;
-    dijkstra(board, result, start.first, start.second, -1);
+    int distance = 0;
+    while(!q.empty()) {
+        auto q_front = q.front();
+        if (q_front.first > 0 && q_front.first < n && q_front.second > 0 && q_front.second < n){
+            if (board[q_front.first][q_front.second] != 0 && result[q_front.first][q_front.second] > distance)
+                result[q_front.first][q_front.second] == distance;
+        }
+        q.pop();
+        ++distance;
+        q.push(make_pair(q_front.first - 1, q_front.second));
+        q.push(make_pair(q_front.first + 1, q_front.second));
+        q.push(make_pair(q_front.first, q_front.second - 1));
+        q.push(make_pair(q_front.first, q_front.second + 1));
+    }
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
             printf("%d ", result[i][j]);
